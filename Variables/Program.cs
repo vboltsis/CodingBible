fe0be7d751d008a9fbf2c3f7940c7481e43c8621
EVANGELOS BOLTSIS <vbk1313@hotmail.com>
2021-07-28 21:46:31 +0300

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type of variable -- name of variable = value
+            int num1 = -1;
+            int num2 = 2;
+
+            int maxValue = 2147483647;
+            int minValue = -2147483648;
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
